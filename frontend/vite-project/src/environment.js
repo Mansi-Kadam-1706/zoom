5afb589dc2bf64e8d4100775161e6d338c79c034
Mansi-kadam-1706 <kadammansi1706@gmail.com>
2026-05-03 15:04:39 +0530

    fix: environment syntax error

--- frontend/vite-project/src/environment.js
+++ frontend/vite-project/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-"https://apnabackend-cqqo.onrender.com/" :
 
-     "http://localhost:8080",
-   
+const server = IS_PROD
+  ? "https://apnabackend-cqqo.onrender.com/"
+  : "http://localhost:8080";
 
 export default server;
