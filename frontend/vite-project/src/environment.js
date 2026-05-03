24b85921721aeffd36fd24d8805b903d1f47a6f0
Mansi-kadam-1706 <kadammansi1706@gmail.com>
2026-05-03 19:07:41 +0530

    fix: updated backend routes

--- frontend/vite-project/src/environment.js
+++ frontend/vite-project/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://apnabackend-cqqo.onrender.com/"
+  ? "https://apnabackend-cqqo.onrender.com"
   : "http://localhost:8080";
 
 export default server;
